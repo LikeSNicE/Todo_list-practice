7538a0fc51c8d3b388808bfe9e0acc82f2fc940c
Nikita Lutsenko <nik.luzenko@mail.ru>
2025-04-11 18:39:11 +0500

    Все задачи из issue были добавлены

---
+++ src/api/Baseapi.ts
@@ -0,0 +1,3 @@
+const base_url:string = "https://20a93d733216cc02.mokky.dev";
+
+export { base_url };
--- src/components/TasksList.vue
+++ src/components/TasksList.vue
@@ -1,47 +1,43 @@
-<script  setup>
-import { ref } from "vue";
+<script setup>
+import { ref, onMounted } from "vue";
 import Task from "./Task.vue";
+import axios from "axios";
+import { base_url } from "../api/Baseapi";
 
-const tasks = ref([
-  {
-    id: 1,
-    title: "Купить продукты",
-    isDone: false,
-  },
-  {
-    id: 2,
-    title: "Изучить TypeScript",
-    isDone: false,
-  },
-  {
-    id: 3,
-    title: "Поиграть The Witcher 3 wild hunt",
-    isDone: false,
-  },
-]);
+const tasks = ref([]);
+
+const getAlltask = async () => {
+  try {
+    const { data } = await axios.get(`${base_url}/tasks`);
+    tasks.value = data;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+onMounted(getAlltask)
 </script>
 
 <template>
-  <p
-  class="mt-[2em] text-center font-semibold text-xl"
-  v-if="!tasks.length">На данный момент задач не имеется :D</p>
-  <ul class="mt-[2em] flex flex-col gap-4 justify-center items-center
-  ">
-    <li class="w-3/5 task-item "  v-for="task in tasks">
+  <p class="mt-[2em] text-center font-semibold text-xl" v-if="!tasks.length">
+    На данный момент задач не имеется :D
+  </p>
+  <ul class="mt-[2em] flex flex-col gap-4 justify-center items-center">
+    <li class="w-3/5 task-item" v-for="task in tasks">
       <Task :title="task.title" :key="task.id" />
     </li>
   </ul>
 </template>
 
 <style scoped lang="scss">
-  .task-item {
+.task-item {
   @media (max-width: 1370px) {
     width: 50%;
   }
-  @media(max-width: 1025px){
+  @media (max-width: 1025px) {
     width: 75%;
   }
-  @media(max-width: 768px){
+  @media (max-width: 768px) {
     width: 100%;
   }
 }
