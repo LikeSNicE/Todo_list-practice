15875fcdb7a393db5a14d5fc5ef1c6b3cc23b945
Nikita Lutsenko <nik.luzenko@mail.ru>
2025-05-26 21:25:06 +0500

    fix start white screen

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,4 @@
-<script setup >
-
+<script setup>
 import { useRouter } from "vue-router";
 import { useLoadingStore } from "../stores/loadingStore";
 import { useModalStore } from "../stores/modalStore";
@@ -10,36 +9,38 @@ import SpinnerLoader from "../components/SpinnerLoader.vue";
 import TaskModal from "../components/TaskModal.vue";
 import TasksList from "../components/TasksList.vue";
 import CategoryModal from "../components/CategoryModal.vue";
-
 import SearchTask from "../components/SearchTask.vue";
 import SortTask from "../components/SortTask.vue";
 import DescriptionModal from "../components/DescriptionModal.vue";
 import TaskFilterSort from "../components/TaskFilterSort.vue";
 
+import { computed, onMounted } from "vue";
+
 const loadingStore = useLoadingStore();
 const modalStore = useModalStore();
 const authStore = useAuthStore();
 
-const isAuthenticated = authStore.authState.isAuthenticated;
-
+const isAuthenticated = computed(() => authStore.authState.isAuthenticated);
 const router = useRouter();
 
-if(!isAuthenticated){
-  router.push('/auth');
-}
-
+onMounted(() => {
+  if (!isAuthenticated.value) {
+    router.push("/auth");
+  }
+});
 </script>
+
 <template>
   <div v-if="isAuthenticated && !loadingStore.isLoading">
-    <TaskFilterSort/>
+    <TaskFilterSort />
     <TasksList />
     <TaskModal v-if="modalStore.activeModal === 'task'" />
     <CategoryModal v-if="modalStore.activeModal === 'category'" />
-    <DescriptionModal v-if="modalStore.activeModal === 'description'"/>
+    <DescriptionModal v-if="modalStore.activeModal === 'description'" />
     <BtnModal />
   </div>
 
   <div v-else>
-     <SpinnerLoader/>
+    <SpinnerLoader />
   </div>
 </template>
