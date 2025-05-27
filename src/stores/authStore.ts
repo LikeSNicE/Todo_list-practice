42e7686a2516b154d9b7fbb76526f53e4d5fd9f1
Nikita Lutsenko <nik.luzenko@mail.ru>
2025-05-27 09:47:56 +0500

    выполнил 6 задачу

--- src/stores/authStore.ts
+++ src/stores/authStore.ts
@@ -16,8 +16,6 @@ export const useAuthStore = defineStore("auth", () => {
   const checkAuth = async (): Promise<User | null> => {
     const token = localStorage.getItem("token");
 
-    if(!token) return null;
-
     try {
       const { data } = await api.get("/auth_me", {
         headers: {
